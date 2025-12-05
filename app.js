06a4bb7e399b7dc92662443bfb430fd5fe0f0ed0
Subodh <poudelsubodh878@gmail.com>
2025-12-05 15:30:11 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+//Alternative
+// const app = require('express')()
+
+app.get("/",(req, res)=>{
+    res.send("Bye world")
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Nodejs server has started at port 3000")
+})
+
